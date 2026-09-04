3c7146cd3329d6cdcaa8bdbc3156e8a199117c4e
Александра Булыга <likeroman803@gmail.com>
2026-09-05 00:03:26 +0300

    Added student info and Pi calculation

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const http = require('http');
 
+
+function calculatePi(digits) {
+    let pi = 0;
+    for (let k = 0; k < 1000000; k++) {
+        pi += (k % 2 === 0 ? 1 : -1) / (2 * k + 1);
+    }
+    pi *= 4;
+    return pi.toFixed(digits); // digits - ваш номер журнала (2)
+}
+
+const fio = "Булыга Александра Витальевна";
+const group = "477";
+const journalNumber = 2; 
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end('<h1>Привет, мир!</h1>');
+   
+    res.end(`<h1>${fio}</h1><p>Группа: ${group}</p><p>Число Пи (${journalNumber} знака): ${calculatePi(journalNumber)}</p>`);
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-console.log(`Сервер запущен на http://localhost:${PORT}`);
+server.listen(3000, () => {
+    console.log('Сервер запущен на порту 3000');
 });
